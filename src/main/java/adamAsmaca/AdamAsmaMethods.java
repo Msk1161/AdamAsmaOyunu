90b0b287eb1b2023e26ec4d2cf446c6e9ff7c39a
QAbatch81 <mskn1161@gmail.com>
2023-07-18 10:07:57 +0100

    AdamAsma_V2

--- src/main/java/adamAsmaca/AdamAsmaMethods.java
+++ src/main/java/adamAsmaca/AdamAsmaMethods.java
@@ -1,7 +1,6 @@
 package adamAsmaca;
 
 import javax.swing.*;
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,8 +14,12 @@ public class AdamAsmaMethods {
     List<String> turkceKelimeler = turkceKelimeOku(turkceKelimeYolu);
     int idxKelime;
     static int hataSayisi=0;
-
+    String kelime;
+    int kelimeSayisi=0;
+    StringBuilder kelimeBul= new StringBuilder();
+    Scanner scan = new Scanner(System.in);
     public void giris(){
+
         System.out.println("=========== ADAM ASMA OYUNU ===========");
         System.out.println("||                                   ||");
         System.out.println("||   Adam Asma oyununa hoşgeldiniz.  ||");
@@ -25,32 +28,19 @@ public class AdamAsmaMethods {
         System.out.println("============> made by msk <============");
         System.out.println("");
         System.out.println("===>>> OYUN YÜKLENİYOR <<<====");
-        bekletme(10);
+        bekletme(5);
         sayiUret();
+
         System.out.println(idxKelime);
         System.out.println(turkceKelimeler.get(idxKelime));
-        String kelime=turkceKelimeler.get(idxKelime);
-        int kelimeSayisi=0;
-        for (int i = 0; i <kelime.length() ; i++) {
-            if(kelime.substring(i,i+1).equals(" ")){
-                kelimeSayisi++;
-            }
-        }
-        int harfSayisi=kelime.length()-kelimeSayisi;
-        System.out.println("Metin, "+(kelimeSayisi+1) + " kelime ve "+harfSayisi+" harften oluşuyor.");
-        StringBuilder kelimeBul= new StringBuilder();
-        for (int i = 0; i <kelime.length() ; i++) {
-            if(kelime.substring(i,i+1).equals(" ")){
-                kelimeBul.insert(i," ");
-            }else{
-                kelimeBul.insert(i,"_");
-            }
-        }
-        System.out.println(kelimeBul);
+
+        kelime=turkceKelimeler.get(idxKelime);
+        kelimeIncele(kelime);
+
         String harf;
-        Scanner scan = new Scanner(System.in);
         int harfCounter=0;
         List<String> lst =new ArrayList();
+
         do{
             int hataCounter=0;
             System.out.print("lutfen bir harf giriniz : ");
@@ -69,6 +59,7 @@ public class AdamAsmaMethods {
                 }
                 System.out.println(kelimeBul);
                 System.out.println(harfCounter);
+
                 if(hataCounter==0){
                     hataSayisi++;
                     System.out.println("Hata sayısı : "+hataSayisi);
@@ -81,14 +72,40 @@ public class AdamAsmaMethods {
                 }
             }
 
-
-    }while (hataSayisi<9 && harfCounter<kelime.length());
-        if (harfCounter==kelime.length()){
-            System.out.println("Oyun bitti kazandiniz");
-        }else{
-            System.out.println("Oyun bitti bu seferlik kaybettiniz");
+    }while (hataSayisi<9 && harfCounter<kelime.length()-kelimeSayisi);
+        System.out.println(harfCounter == kelime.length()-kelimeSayisi ? "Oyun bitti kazandiniz" : "Oyun bitti bu seferlik kaybettiniz");
+        cikisDevam();
+    }
+    private void cikisDevam() {
+        kelimeBul.delete(0,kelime.length());
+        System.out.print("Devam etmek istiyormusunuz? Devam etmek için 'E', çıkmak için herhangi bir tuşa basınız : ");
+        String sorun = scan.next().substring(0, 1).toUpperCase();
+        if (sorun.equals("E")) {
+            giris();
+        } else {
+            cikis();
         }
+    }
+    private void cikis(){
+        System.out.println("Tebrikler iyi oynadınız. Oyundan Çıktınız.");
+    }
+    private void kelimeIncele(String kelime){
 
+        for (int i = 0; i < this.kelime.length() ; i++) {
+            if(this.kelime.substring(i,i+1).equals(" ")){
+                kelimeSayisi++;
+            }
+        }
+        int harfSayisi= this.kelime.length()-kelimeSayisi;
+        System.out.println("Metin, "+(kelimeSayisi+1) + " kelime ve "+harfSayisi+" harften oluşuyor.");
+        for (int i = 0; i < this.kelime.length() ; i++) {
+            if(this.kelime.substring(i,i+1).equals(" ")){
+                kelimeBul.insert(i," ");
+            }else{
+                kelimeBul.insert(i,"_");
+            }
+        }
+        System.out.println(kelimeBul);
     }
    public List<String> turkceKelimeOku(String turkceKelimeYolu){
        List<String> satirlistesi = new ArrayList<>();
